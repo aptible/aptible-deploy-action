af4858547a26d358618ea2a4cc1e2f04c98e2c1b
bayley-aptible <62611769+bayley-aptible@users.noreply.github.com>
2020-05-01 13:12:08 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,39 +1,39 @@
 #!/usr/bin/env bash
 set -o errexit
 
-if [ -z "$APTIBLE_ROBOT_USERNAME" ]; then
-  echo "Aborting: APTIBLE_ROBOT_USERNAME is not set"
+if [ -z "$INPUT_USERNAME" ]; then
+  echo "Aborting: username is not set"
   exit 1
 fi
 
-if [ -z "$APTIBLE_ROBOT_PASSWORD" ]; then
-  echo "Aborting: APTIBLE_ROBOT_PASSWORD is not set"
+if [ -z "$INPUT_PASSWORD" ]; then
+  echo "Aborting: password is not set"
   exit 1
 fi
 
-if [ -z "$APTIBLE_ENVIRONMENT" ]; then
-  echo "Aborting: APTIBLE_ENVIRONMENT is not set"
+if [ -z "$INPUT_ENVIRONMENT" ]; then
+  echo "Aborting: environment is not set"
   exit 1
 fi
 
-if [ -z "$APTIBLE_APP" ]; then
-  echo "Aborting: APTIBLE_APP is not set"
+if [ -z "$INPUT_APP" ]; then
+  echo "Aborting: app is not set"
   exit 1
 fi
 
-if [ -z "$DOCKER_IMG" ]; then
-  echo "Aborting: DOCKER_IMG is not set"
+if [ -z "$INPUT_DOCKER_IMG" ]; then
+  echo "Aborting: docker_img is not set"
   exit 1
 fi
 
 aptible login \
-  --email "$APTIBLE_ROBOT_USERNAME" \
-  --password "$APTIBLE_ROBOT_PASSWORD"
+  --email "$INPUT_USERNAME" \
+  --password "$INPUT_PASSWORD"
 
-if ! APTIBLE_OUTPUT_FORMAT=json aptible apps | jq -e ".[] | select(.handle == \"$APTIBLE_APP\") | select(.environment.handle == \"$APTIBLE_ENVIRONMENT\")" > /dev/null; then
-  echo "$0: skip (could not find app $APTIBLE_APP)" >&2
+if ! APTIBLE_OUTPUT_FORMAT=json aptible apps | jq -e ".[] | select(.handle == \"$INPUT_APP\") | select(.environment.handle == \"$INPUT_ENVIRONMENT\")" > /dev/null; then
+  echo "$0: skip (could not find app $INPUT_APP)" >&2
   exit 0
 fi
 
-aptible deploy --environment "$APTIBLE_ENVIRONMENT" \
-               --app "$APTIBLE_APP" --docker-image "$DOCKER_IMG"
+aptible deploy --environment "$INPUT_ENVIRONMENT" \
+               --app "$INPUT_APP" --docker-image "$INPUT_DOCKER_IMG"
