0d259138e54d20762354cf3fbfe999f10d076200
michaelwang13 <michael.wang@aptible.com>
2025-04-02 08:24:31 -0400

    Update Git to ignore directory warnings (#34)
    * mark all directories as safe
    
    * globally trust github/workspace directory.
    
    * update to current directory
    
    * check github workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -61,6 +61,7 @@ if [ "$INPUT_TYPE" == "git" ]; then
   fi
   export ACCESS_TOKEN=$(cat "$HOME/.aptible/tokens.json" | jq ".[\"$APTIBLE_AUTH_ROOT_URL\"]" -r)
   REMOTE_URL="root@$INPUT_GIT_REMOTE:$INPUT_ENVIRONMENT/$INPUT_APP.git"
+  git config --global --add safe.directory $GITHUB_WORKSPACE
   git remote add aptible ${REMOTE_URL}
   REMOTE_BRANCH="deploy-$(date "+%s")"
   GIT_SSH_COMMAND="ssh -o SendEnv=ACCESS_TOKEN -o PubkeyAuthentication=no -o UserKnownHostsFile=./known_hosts -p 43022" git push aptible "$BRANCH:$REMOTE_BRANCH"
